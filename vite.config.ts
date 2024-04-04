242d284bf49121c8c41a5b070b16ade66cda8490
bquangDinh <buiquangdinh1751@gmail.com>
2024-04-03 21:36:31 -0400

    Build for production

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
 import removeConsole from "vite-plugin-remove-console";
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), removeConsole()],
+  plugins: [react(), svgr(), removeConsole(), createHtmlPlugin({
+	minify: true
+  })],
   resolve: {
     alias: {
       "@src": "/src",
@@ -13,4 +16,8 @@ export default defineConfig({
       "@components": "/src/components",
     },
   },
+  build: {
+	assetsDir: './',
+	minify: 'terser'
+  }
 })
